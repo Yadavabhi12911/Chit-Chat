cda8b0b27232687333e02a442306dd020e3e5c3e
yadavabhi12911 <yadavabhi12911@gmail.com>
2026-01-19 21:57:23 +0530

    mesage with ttl and friendRequest schema design

---
+++ backend/models/message.models.js
@@ -0,0 +1,46 @@
+
+import mongoose, { model, Schema } from "mongoose";
+
+const messageSchema = new Schema({
+    senderId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+        index: true
+    },
+    receiverId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+        index: true
+    },
+    content: {
+        type: String,
+        required: true,
+        maxlength: [2000, " message length limit exceed !"]
+    },
+    readAt: {
+        type: Date,
+        default: null,
+        index: true,
+
+    },
+    expiredAt: {
+        type: Date,
+        required: true,
+        index: true
+    }
+}, { timestamps: true })
+
+messageSchema.index(
+    { expiredAt: 1 },
+    { expireAfterSeconds: 0 }
+
+)
+
+messageSchema.index({ senderId: 1, receiverId: 1, createdAt: -1 })
+
+
+export const Message = model("Message", messageSchema)
+
+
