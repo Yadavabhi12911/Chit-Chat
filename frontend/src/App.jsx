fbe4a9b06cfd9e5f4636555986f3892ac1844f52
yadavabhi12911 <yadavabhi12911@gmail.com>
2026-01-23 21:17:51 +0530

    test and setup websocket

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { myRoutes } from "./context/MyRoute.jsx";
 
 
 const App = () => {
+
   return (
     <div >
       <h1 className="text-3xl font-bold text-blue-950 text-center  bg-blue-200 p-4">Welcome to ChitChat...</h1>
